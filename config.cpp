458802ea6280010e224b1bd4c723c477301527d0
Manish Chaurasiya <mv0386410@gmail.com>
2026-01-02 12:58:48 +0530

    Trying to understand and learn github

--- config.cpp
+++ config.cpp
@@ -7,7 +7,7 @@ int main()
     cout<<"git - Version 2"<<endl;
     cout<<"git -Version 3 Added few more line of code "<<endl;
     cout<<"this changes is done after the previous version are push to the GitHub"<<endl;
-
+    cout <<"Getting erroe in the terminal and ,the problem is main branch is down and the head is in the upper ... not able to understand the git .... frustead but trying the learn and i will definitly learn it ... ";
 
    return 0;
 }
